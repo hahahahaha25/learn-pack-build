853d360e6ec52539f7501a61bb52e9a07b5f94b8
sun <2232525679@qq.com>
2024-12-26 17:15:18 +0800

    学习rollup和esbuild简单demo

---
+++ rollup/rollup.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  input: "src/main.js",
+  output: {
+    file: "src/bundle.js",
+    format: "cjs",
+  },
+};
---
+++ rollup/src/foo.js
@@ -0,0 +1,2 @@
+export default "hello rollup!";
+let demo = "invalid date"; //无用代码
